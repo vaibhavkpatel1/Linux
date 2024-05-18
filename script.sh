e2443f62e9ac457ee3dfc3e70cd1beb8eb8fd10d
Vaibhav Patel <48453838+vaibhavkpatel1@users.noreply.github.com>
2024-05-18 11:04:56 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get install gparted
+sudo apt-get install synaptic
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+sudo snap install vlc
+sudo snap install octave
+sudo snap install chromium
+sudo snap install teams-for-linux
+sudo snap install clipaste
+sudo snap install onlyoffice-desktopeditors 
+sudo snap install thunderbird
+sudo snap install code
+sudo snap install xdman
+sudo snap install motrix
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+
